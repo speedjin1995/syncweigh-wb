04edd6086e46fcfe63c44851af985bbcddbc2eb7
JihB_0105 <ngjihbin1234@gmail.com>
2025-09-28 12:53:44 +0800

    added allow manual and deduct to user

--- SQL/deploy.sql
+++ SQL/deploy.sql
@@ -803,3 +803,46 @@ CREATE OR REPLACE TRIGGER `TRG_UPD_USER` BEFORE UPDATE ON `Users` FOR EACH ROW B
 END
 $$
 DELIMITER ;
+
+DELIMITER ;
+
+-- 28/09/2025 --
+ALTER TABLE `Users` ADD `allow_manual` VARCHAR(1) NOT NULL DEFAULT 'N' AFTER `plant_id`;
+
+ALTER TABLE `Users_Log` ADD `allow_manual` VARCHAR(1) NOT NULL DEFAULT 'N' AFTER `plant_id`;
+
+ALTER TABLE `Users` ADD `allow_deduct` VARCHAR(1) NOT NULL DEFAULT 'N' AFTER `allow_manual`;
+
+ALTER TABLE `Users_Log` ADD `allow_deduct` VARCHAR(1) NOT NULL DEFAULT 'N' AFTER `allow_manual`;
+
+DELIMITER $$
+CREATE OR REPLACE TRIGGER `TRG_INS_USER` AFTER INSERT ON `Users` FOR EACH ROW 
+INSERT INTO Users_Log (
+    user_id, employee_code, username, name, useremail, password, plant_id, allow_manual, allow_deduct, `status`, action_id, action_by, event_date
+) 
+VALUES (
+    NEW.id, NEW.employee_code, NEW.username, NEW.name, NEW.useremail, NEW.password, NEW.plant_id, NEW.allow_manual, NEW.allow_deduct, NEW.status, 1, NEW.created_by, NEW.created_date
+)
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE OR REPLACE TRIGGER `TRG_UPD_USER` BEFORE UPDATE ON `Users` FOR EACH ROW BEGIN
+    DECLARE action_value INT;
+
+    -- Check if status = 1, set action_id to 3, otherwise set to 2
+    IF NEW.status = 1 THEN
+        SET action_value = 3;
+    ELSE
+        SET action_value = 2;
+    END IF;
+
+    -- Insert into Users_Log table
+    INSERT INTO Users_Log (
+        user_id, employee_code, username, name, useremail, password, plant_id, allow_manual, allow_deduct, `status`, action_id, action_by, event_date
+    ) 
+    VALUES (
+        NEW.id, NEW.employee_code, NEW.username, NEW.name, NEW.useremail, NEW.password, NEW.plant_id, NEW.allow_manual, NEW.allow_deduct, NEW.status, action_value, NEW.modified_by, NEW.modified_date
+    );
+END
+$$
+DELIMITER ;
--- php/getUser.php
+++ php/getUser.php
@@ -28,6 +28,8 @@ if(isset($_POST['userID'])){
                 $message['useremail'] = $row['useremail'];
                 $message['role_code'] = $row['role'];
                 $message['plant'] = $row['plant_id'];
+                $message['allow_manual'] = $row['allow_manual'];
+                $message['allow_deduct'] = $row['allow_deduct'];
             }
             
             echo json_encode(
