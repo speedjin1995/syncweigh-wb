c6e2c39461014bbed14e9856851ce7a550dc30b2
speedjin1995 <speedjin1995@gmail.com>
2025-11-09 18:51:29 +0800

    Update database credentials in config.php

--- layouts/config.php
+++ layouts/config.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'u664110560_plytech');
+define('DB_USERNAME', 'u735381057_plytech');
 define('DB_PASSWORD', '@Sync5500');
-define('DB_NAME', 'u664110560_plytech');
+define('DB_NAME', 'u735381057_plytech');
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
