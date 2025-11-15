fc473be7be12df2241b443d6d90a0bd52edd11c4
speedjin1995 <speedjin1995@gmail.com>
2025-11-16 00:19:08 +0800

    asdds

--- php/db_connect.php
+++ php/db_connect.php
@@ -6,8 +6,8 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true || !isset($_S
 }
 else{
     date_default_timezone_set('Asia/Kuala_Lumpur');
-    //$db = mysqli_connect("localhost", "u735381057_plytech", "@Sync5500", "u735381057_plytech");
-    $db = mysqli_connect("srv2051.hstgr.io", "u735381057_plytech", "@Sync5500", "u735381057_plytech");
+    $db = mysqli_connect("localhost", "u735381057_plytech", "@Sync5500", "u735381057_plytech");
+    //$db = mysqli_connect("srv2051.hstgr.io", "u735381057_plytech", "@Sync5500", "u735381057_plytech");
     
     if(mysqli_connect_errno()){
         echo 'Database connection failed with following errors: ' . mysqli_connect_error();
