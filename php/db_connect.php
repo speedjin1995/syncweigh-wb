68c6cbeb0ce7f38ced2ba4ff7afd5f76077437ac
speedjin1995 <speedjin1995@gmail.com>
2025-11-30 19:50:16 +0800

    added hardcoded for testing

--- php/db_connect.php
+++ php/db_connect.php
@@ -6,7 +6,8 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true || !isset($_S
 }
 else{
     date_default_timezone_set('Asia/Kuala_Lumpur');
-    $db = mysqli_connect("localhost", "u178823648_".$_SESSION['company'], "@Sync5500", "u178823648_".$_SESSION['company']);
+    //$db = mysqli_connect("localhost", "u178823648_".$_SESSION['company'], "@Sync5500", "u178823648_".$_SESSION['company']);
+    $db = mysqli_connect("srv1637.hstgr.io", "u178823648_".$_SESSION['company'], "@Sync5500", "u178823648_".$_SESSION['company']);
     
     if(mysqli_connect_errno()){
         echo 'Database connection failed with following errors: ' . mysqli_connect_error();
