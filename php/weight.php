659de336eb157c848b0980fa0a81922b84a316b9
Ng Jih Bin <sky.ng@dcap.my>
2025-12-07 21:39:51 +0800

    fixed totalPrice null bug

--- php/weight.php
+++ php/weight.php
@@ -437,7 +437,7 @@ if (isset($_POST['transactionId'], $_POST['transactionStatus'], $_POST['weightTy
     }
 
     if (empty($_POST["totalPrice"])) {
-        $totalPrice = null;
+        $totalPrice = 0.00;
     } else {
         $totalPrice = trim($_POST["totalPrice"]);
     }
