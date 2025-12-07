b1b82dfea015f8151de304d5b3cadf8f56b0e2f8
RenaYew <renayewhy@yahoo.com>
2025-12-07 21:21:40 +0800

    Logo Wrong Spelling
    - correct the spelling

--- layouts/sidebar.php
+++ layouts/sidebar.php
@@ -32,7 +32,7 @@
                 <img src="assets/images/east-logo-icon.png" alt="" height="35">
             </span>
             <span class="logo-lg">
-                <img src="assets/images/east-logo-dark.png" alt="" height="35">
+                <img src="assets/images/east-logo.png" alt="" height="35">
             </span>
         </a>
         <!-- Light Logo-->
@@ -41,7 +41,7 @@
                 <img src="assets/images/east-logo-icon.png" alt="" height="35">
             </span>
             <span class="logo-lg">
-                <img src="assets/images/east-logo-dark.png" alt="" height="35">
+                <img src="assets/images/east-logo.png" alt="" height="35">
             </span>
         </a>
         <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover"
