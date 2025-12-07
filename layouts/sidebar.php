ce4016d6a9d7f7e5c3ebc647637ea4a0e39d01fa
RenaYew <renayewhy@yahoo.com>
2025-12-07 21:07:40 +0800

    change logo
    - change logo
    - adjust logo size

--- layouts/sidebar.php
+++ layouts/sidebar.php
@@ -26,7 +26,24 @@
                 ';
             }
         ?>
-        
+        <!-- Dark Logo-->
+        <a href="index.php" class="logo logo-dark">
+            <span class="logo-sm">
+                <img src="assets/images/east-logo-icon.png" alt="" height="35">
+            </span>
+            <span class="logo-lg">
+                <img src="assets/images/east-logo-dark.png" alt="" height="35">
+            </span>
+        </a>
+        <!-- Light Logo-->
+        <a href="index.php" class="logo logo-light">
+            <span class="logo-sm">
+                <img src="assets/images/east-logo-icon.png" alt="" height="35">
+            </span>
+            <span class="logo-lg">
+                <img src="assets/images/east-logo-dark.png" alt="" height="35">
+            </span>
+        </a>
         <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover"
             id="vertical-hover">
             <i class="ri-record-circle-line"></i>
