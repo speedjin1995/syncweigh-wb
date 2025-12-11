5042697f8c6284550d52c7ff7d3ffed3deba678b
Ng Jih Bin <sky.ng@dcap.my>
2025-12-11 23:13:07 +0800

    hide pricing field if includePrice is N

--- product.php
+++ product.php
@@ -3,6 +3,19 @@
 
 <?php
     $rawMaterial = $db->query("SELECT * FROM Raw_Mat WHERE status = '0'");
+
+    // Get Company Detail
+    $stmt = $db->prepare("SELECT * from Company WHERE id = 1");
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    $includePrice = '';
+    $includeContainer = '';
+    if(($row = $result->fetch_assoc()) !== null){
+        $includePrice = $row['include_price'];
+        $includeContainer = $row['include_container'];
+    }
+
 ?>
 
 <head>
@@ -154,7 +167,7 @@
                                                                                     </div>
                                                                                 </div>
                                                                             </div>
-                                                                            <div class="col-xxl-12 col-lg-12 mb-3">
+                                                                            <div class="col-xxl-12 col-lg-12 mb-3" <?php if($includePrice == 'N'){ echo 'style="display:none;"'; } ?>>
                                                                                 <div class="row">
                                                                                     <label for="productPrice" class="col-sm-4 col-form-label">Product Price</label>
                                                                                     <div class="col-sm-8">
@@ -334,7 +347,9 @@
                                                                     <th><input type="checkbox" id="selectAllCheckbox" class="selectAllCheckbox"></th>
                                                                     <th>Product Code</th>
                                                                     <th>Product Name</th>
+                                                                    <?php if($includePrice == 'Y'){ ?>
                                                                     <th>Product Price</th>
+                                                                    <?php } ?>
                                                                     <th>Description</th>
                                                                     <th><?=$languageArray['status_code'][$language]?></th>
                                                                     <th><?=$languageArray['action_code'][$language]?></th>
@@ -469,7 +484,9 @@ $(function () {
             },
             { data: 'product_code' },
             { data: 'name' },
+            <?php if($includePrice == 'Y'): ?>
             { data: 'price' },
+            <?php endif; ?>
             { data: 'description' },
             { 
                 data: 'id',
--- rawMaterial.php
+++ rawMaterial.php
@@ -1,6 +1,19 @@
 <?php include 'layouts/session.php'; ?>
 <?php include 'layouts/head-main.php'; ?>
 
+<?php
+    // Get Company Detail
+    $stmt = $db->prepare("SELECT * from Company WHERE id = 1");
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    $includePrice = '';
+    $includeContainer = '';
+    if(($row = $result->fetch_assoc()) !== null){
+        $includePrice = $row['include_price'];
+        $includeContainer = $row['include_container'];
+    }
+?>
 <head>
     <title>Weighing | Synctronix - Weighing System</title>
     <?php include 'layouts/title-meta.php'; ?>
@@ -135,7 +148,7 @@
                                                                                     </div>
                                                                                 </div>
                                                                             </div>
-                                                                            <div class="col-xxl-12 col-lg-12 mb-3">
+                                                                            <div class="col-xxl-12 col-lg-12 mb-3" <?php if($includePrice == 'N'){ echo 'style="display:none;"'; } ?>>
                                                                                 <div class="row">
                                                                                     <label for="productPrice" class="col-sm-4 col-form-label">Raw Material Price</label>
                                                                                     <div class="col-sm-8">
@@ -296,7 +309,9 @@
                                                                     <th><input type="checkbox" id="selectAllCheckbox" class="selectAllCheckbox"></th>
                                                                     <th>Raw Material Code</th>
                                                                     <th>Raw Material Name</th>
+                                                                    <?php if($includePrice == 'Y'){ ?>
                                                                     <th>Raw Material Price</th>
+                                                                    <?php } ?>
                                                                     <th>Description</th>
                                                                     <th>Type</th>
                                                                     <th><?=$languageArray['status_code'][$language]?></th>
@@ -405,7 +420,9 @@ $(function () {
             },
             { data: 'raw_mat_code' },
             { data: 'name' },
+            <?php if($includePrice == 'Y'): ?>
             { data: 'price' },
+            <?php endif; ?>
             { data: 'description' },
             { data: 'type' },
             { 
