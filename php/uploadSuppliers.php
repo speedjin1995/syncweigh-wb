11c813a4c0a59eb96fe0abaf9c14f0949aaa797f
Ng Jih Bin <sky.ng@dcap.my>
2025-12-11 22:06:57 +0800

    added new field to upload customer and supplier

--- php/uploadSuppliers.php
+++ php/uploadSuppliers.php
@@ -25,6 +25,7 @@ if (!empty($data)) {
         $ContactName = !empty($rows['ContactName']) ? $rows['ContactName'] : '';
         $ICNo = !empty($rows['ICNo']) ? $rows['ICNo'] : '';
         $TinNo = !empty($rows['TinNo']) ? $rows['TinNo'] : '';
+        $Mpob = !empty($rows['Mpob']) ? $rows['Mpob'] : '';
         $action = "1";
         
         # Customer Checking & Processing
@@ -34,17 +35,17 @@ if (!empty($data)) {
             $supplierRow = mysqli_fetch_assoc($supplierDetail);
             
             if(empty($supplierRow)){
-                if ($insert_stmt = $db->prepare("INSERT INTO Supplier (supplier_code, company_reg_no, new_reg_no, name, address_line_1, address_line_2, address_line_3, address_line_4, phone_no, fax_no, contact_name, ic_no, tin_no, created_by, modified_by) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")) {
-                    $insert_stmt->bind_param('sssssssssssssss', $Code, $RegNo, $NewRegNo, $Name, $Address1, $Address2, $Address3, $Address4, $Phone, $Fax, $ContactName, $ICNo, $TinNo, $uid, $uid);
+                if ($insert_stmt = $db->prepare("INSERT INTO Supplier (supplier_code, company_reg_no, new_reg_no, name, address_line_1, address_line_2, address_line_3, address_line_4, phone_no, fax_no, contact_name, ic_no, tin_no, mpob, created_by, modified_by) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")) {
+                    $insert_stmt->bind_param('ssssssssssssssss', $Code, $RegNo, $NewRegNo, $Name, $Address1, $Address2, $Address3, $Address4, $Phone, $Fax, $ContactName, $ICNo, $TinNo, $Mpob, $uid, $uid);
                     $insert_stmt->execute();
                     $invid = $insert_stmt->insert_id; // Get the inserted reseller ID
                     $insert_stmt->close();
 
-                    if ($insert_log = $db->prepare("INSERT INTO Supplier_Log (supplier_id, supplier_code, company_reg_no, new_reg_no, name, address_line_1, address_line_2, address_line_3, address_line_4, phone_no, fax_no, contact_name, ic_no, tin_no, action_id, action_by) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")) {
-                        $insert_log->bind_param('ssssssssssssssss', $invid, $Code, $RegNo, $NewRegNo, $Name, $Address1, $Address2, $Address3, $Address4, $Phone, $Fax, $ContactName, $ICNo, $TinNo, $action, $uid);
-                        $insert_log->execute();
-                        $insert_log->close();
-                    }            
+                    // if ($insert_log = $db->prepare("INSERT INTO Supplier_Log (supplier_id, supplier_code, company_reg_no, new_reg_no, name, address_line_1, address_line_2, address_line_3, address_line_4, phone_no, fax_no, contact_name, ic_no, tin_no, action_id, action_by) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")) {
+                    //     $insert_log->bind_param('ssssssssssssssss', $invid, $Code, $RegNo, $NewRegNo, $Name, $Address1, $Address2, $Address3, $Address4, $Phone, $Fax, $ContactName, $ICNo, $TinNo, $action, $uid);
+                    //     $insert_log->execute();
+                    //     $insert_log->close();
+                    // }            
                 }
             }else{
                 $errMsg = "Supplier: ". $Name ." already exist in master data.";
