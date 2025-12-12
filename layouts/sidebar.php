ac36404ac75618262d2684ea9d7039a9e74fa624
speedjin1995 <speedjin1995@gmail.com>
2025-12-12 08:45:04 +0800

    changed logo

--- layouts/sidebar.php
+++ layouts/sidebar.php
@@ -5,19 +5,19 @@
         <!-- Dark Logo-->
         <a href="index.php" class="logo logo-dark">
             <span class="logo-sm">
-                <img src="assets/images/logo-sm.png" alt="" height="40">
+                <img src="assets/images/logo/favicon.png" alt="" height="40">
             </span>
             <span class="logo-lg">
-                <img src="assets/images/logo-lg.png" alt="" height="110">
+                <img src="assets/images/logo/logo1.png" alt="" height="110">
             </span>
         </a>
         <!-- Light Logo-->
         <a href="index.php" class="logo logo-light">
             <span class="logo-sm">
-                <img src="assets/images/logo-sm.png" alt="" height="40">
+                <img src="assets/images/logo/favicon.png" alt="" height="40">
             </span>
             <span class="logo-lg">
-                <img src="assets/images/logo-lg.png" alt="" height="110">
+                <img src="assets/images/logo/logo1.png" alt="" height="110">
             </span>
         </a>
         <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover"
