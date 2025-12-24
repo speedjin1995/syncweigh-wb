83eadb3521d5a3b8498986f06106c99eb2c53aa9
speedjin1995 <speedjin1995@gmail.com>
2025-12-24 22:33:12 +0800

    Fix sidebar item closing tag in sidebar.php

--- layouts/sidebar.php
+++ layouts/sidebar.php
@@ -147,7 +147,7 @@
                             <li class="nav-item">
                                 <li class="nav-item">
                                     <a href="weighingReport.php" class="nav-link"><b><?=$languageArray['weighing_report_code'][$language]?></b></a>
-                                </li-->
+                                </li>
                                 <!--li class="nav-item">
                                     <a href="salesReport.php" class="nav-link"><?=$lang['t-soReport']?></a>
                                 </li>
