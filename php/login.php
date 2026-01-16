1f8ff9d44764e63b560f9826597c40ed64b0022d
Jihb0105 <ngjihbin1234@gmail.com>
2026-01-16 09:56:53 +0800

    added condition to not allow status = 1

--- php/login.php
+++ php/login.php
@@ -35,7 +35,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Validate credentials
     if (empty($username_err) && empty($password_err)) {
         // Prepare a select statement
-        $sql = "SELECT id, employee_code, username, password, role, plant_id FROM Users WHERE username = ?";
+        $sql = "SELECT id, employee_code, username, password, role, plant_id FROM Users WHERE username = ? AND status = 0";
         
         if ($stmt = mysqli_prepare($link, $sql)) {
             // Bind variables to the prepared statement as parameters
