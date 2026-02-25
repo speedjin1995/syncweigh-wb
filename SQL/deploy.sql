6888b5332b790b4f342660032a6ef7952e092803
Ng Jih Bin <sky.ng@dcap.my>
2026-02-25 23:16:15 +0800

    pushed include_grading to company table

--- SQL/deploy.sql
+++ SQL/deploy.sql
@@ -1860,3 +1860,25 @@ CREATE OR REPLACE TRIGGER `TRG_UPD_COMPANY` BEFORE UPDATE ON `Company` FOR EACH 
 END
 $$
 DELIMITER ;
+
+-- 25/02/2026 --
+ALTER TABLE `Company` ADD `include_grading` VARCHAR(1) NOT NULL DEFAULT 'N' AFTER `include_display_setup`;
+ALTER TABLE `Company_Log` ADD `include_grading` VARCHAR(1) NOT NULL DEFAULT 'N' AFTER `include_display_setup`;
+
+DELIMITER $$
+CREATE OR REPLACE TRIGGER `TRG_UPD_COMPANY` BEFORE UPDATE ON `Company` FOR EACH ROW BEGIN
+    DECLARE action_value INT;
+
+    -- Always set action_id = 2 for update
+    SET action_value = 2;
+
+    -- Insert into Company_Log table
+    INSERT INTO Company_Log (
+        company_id, company_code, company_reg_no, new_reg_no, `name`, address_line_1, address_line_2, address_line_3, phone_no, fax_no, tin_no, mobile_no, package, include_price, include_container, include_display_setup, include_grading, action_id, action_by, event_date
+    ) 
+    VALUES (
+        NEW.id, NEW.company_code, NEW.company_reg_no, NEW.new_reg_no, NEW.name, NEW.address_line_1, NEW.address_line_2, NEW.address_line_3, NEW.phone_no, NEW.fax_no, NEW.tin_no, NEW.mobile_no, NEW.package, NEW.include_price, NEW.include_container, NEW.include_display_setup, NEW.include_grading, action_value, NEW.modified_by, NEW.modified_date
+    );
+END
+$$
+DELIMITER ;
