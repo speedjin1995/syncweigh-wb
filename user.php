6bf16c4566f86a17ba38b942c1f5183e33df55a1
Ng Jih Bin <sky.ng@dcap.my>
2026-05-21 00:13:41 +0800

    fixed role not trigger when edit

--- user.php
+++ user.php
@@ -631,7 +631,7 @@ mysqli_stmt_bind_result($stmt4, $pcode, $pname);
                 $('#addModal').find('#username').val(obj.message.username);
                 $('#addModal').find('#name').val(obj.message.name);
                 $('#addModal').find('#useremail').val(obj.message.useremail);
-                $('#addModal').find('#roles').val(obj.message.role_code);
+                $('#addModal').find('#roles').val(obj.message.role_code).trigger("change");
                 $("#addModal").find("#plantId").val(JSON.parse(obj.message.plant)).trigger("change");
 
                 // Remove Validation Error Message
