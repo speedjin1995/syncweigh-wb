62f33cdb5cc9df184a2a7cf0d53c7251bf98f6b6
Ng Jih Bin <sky.ng@dcap.my>
2026-05-21 00:10:50 +0800

    change IC to ID No for translation

--- SQL/deploy.sql
+++ SQL/deploy.sql
@@ -908,3 +908,4 @@ DELIMITER ;
 -- 20/05/2026 --
 INSERT INTO `message_resource` (`message_key_code`, `en`, `zh`, `my`, `ne`) VALUES ('clear_all_code', 'Clear All', '清除全部', 'Kosongkan Semua', 'அனைத்தையும் அழி');
 INSERT INTO `message_resource` (`message_key_code`, `en`, `zh`, `my`, `ne`) VALUES ('cancelled_code', 'Cancelled', '已取消', 'Dibatalkan', 'ரத்து செய்யப்பட்டது');
+UPDATE `message_resource` SET `en`='ID No', `zh`='身份证号码', `my`='No. Kad Pengenalan', `ne`='அடையாள எண்' WHERE `message_key_code`='ic_code';
