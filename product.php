e10675111823bf3f5da88721f82b2f8be155fa3c
Ng Jih Bin <sky.ng@dcap.my>
2026-06-01 12:50:27 +0800

    fixed validation

--- product.php
+++ product.php
@@ -606,6 +606,20 @@
                     });
                     
                     $('#addModal').modal('show');
+
+                    $('#productForm').validate({
+                        errorElement: 'span',
+                        errorPlacement: function (error, element) {
+                        error.addClass('invalid-feedback');
+                        element.closest('.form-group').append(error);
+                        },
+                        highlight: function (element, errorClass, validClass) {
+                        $(element).addClass('is-invalid');
+                        },
+                        unhighlight: function (element, errorClass, validClass) {
+                        $(element).removeClass('is-invalid');
+                        }
+                    });
                 }
                 else if(obj.status === 'failed'){
                     $('#spinnerLoading').hide();
